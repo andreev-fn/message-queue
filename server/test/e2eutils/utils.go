bd49e3905e1fef226017a35b0f42bbdbfa323ea7
Fedor Andreev <f.andreev.dev@gmail.com>
2025-11-12 20:38:29 +0400

    Remove unnecessary "testing.T" from e2e utils

--- server/test/e2eutils/utils.go
+++ server/test/e2eutils/utils.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
-
 	"server/internal/appbuilder"
 	"server/internal/config"
 	"server/internal/domain"
@@ -17,42 +15,45 @@ import (
 )
 
 func PrepareHTTPClient(t *testing.T, app *appbuilder.App) *httpclient.Client {
-	t.Helper()
 	return httpclient.NewClient("/", NewHTTPTestDoer(t, app.Router))
 }
 
 func Prepare(t *testing.T) (*appbuilder.App, *timeutils.StubClock) {
 	testutils.SkipIfNotIntegration(t)
 
-	app, clock := BuildTestApp(t, CreateTestConfig(t))
+	app, clock := BuildTestApp(CreateTestConfig())
 
-	CleanupDatabase(t, app.DB)
+	CleanupDatabase(app.DB)
 
 	return app, clock
 }
 
-func CreateTestConfig(t *testing.T) *config.Config {
-	t.Helper()
-
+func CreateTestConfig() *config.Config {
 	pgConf, err := config.NewPostgresConfig(
 		"127.0.0.1:5432",
 		"queue",
 		"user",
 		"pass",
 	)
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 
 	backoffConfig, err := domain.NewBackoffConfig(
 		[]time.Duration{time.Second * 30},
 		opt.Some(config.DefaultBackoffMaxAttempts),
 	)
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 
 	queueConfig, err := domain.NewQueueConfig(
 		opt.Some(backoffConfig),
 		time.Minute*5,
 	)
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 
 	conf, err := config.NewConfig(
 		opt.Some(pgConf),
@@ -63,37 +64,37 @@ func CreateTestConfig(t *testing.T) *config.Config {
 			domain.UnsafeQueueName("all_results"): queueConfig,
 		},
 	)
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 
 	return conf
 }
 
-func BuildTestApp(t *testing.T, conf *config.Config) (*appbuilder.App, *timeutils.StubClock) {
-	t.Helper()
-
+func BuildTestApp(conf *config.Config) (*appbuilder.App, *timeutils.StubClock) {
 	clock := timeutils.NewStubClock(time.Date(2025, 6, 12, 12, 0, 0, 0, time.Local))
 
 	app, err := appbuilder.BuildApp(conf, &appbuilder.Overrides{
 		Clock: clock,
 	})
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 
 	return app, clock
 }
 
-func CleanupDatabase(t *testing.T, db *sql.DB) {
-	t.Helper()
-
+func CleanupDatabase(db *sql.DB) {
 	if _, err := db.Exec("DELETE FROM messages"); err != nil {
-		require.NoError(t, err)
+		panic(err)
 	}
 	if _, err := db.Exec("DELETE FROM message_payloads"); err != nil {
-		require.NoError(t, err)
+		panic(err)
 	}
 	if _, err := db.Exec("DELETE FROM message_history"); err != nil {
-		require.NoError(t, err)
+		panic(err)
 	}
 	if _, err := db.Exec("DELETE FROM archived_messages"); err != nil {
-		require.NoError(t, err)
+		panic(err)
 	}
 }
